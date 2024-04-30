2a62740b0632d6e32ade28044109bb88387957da
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-04-30 16:22:52 +0200

    bug-fixed

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,7 +2,7 @@ import css from './SearchBox.module.css';
 import { useId } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from '../../redux/filters/slice';
-import { selectNameFilter } from '../../redux/auth/selectors';
+import { selectNameFilter } from '../../redux/filters/selectors';
 
 const SearchBox = () => {
     const dispatch = useDispatch();
--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,14 +1,14 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { selectNameFilter } from "../filters/selectors";
 
-export const selectItems = state => state.contacts.items;
+export const selectAllContacts = (state) => state.contacts.items;
 export const selectError = state => state.contacts.error;
 export const selectLoading = state => state.contacts.loading;
 
 export const selectFilteredContacts = createSelector(
-  [selectItems, selectNameFilter],
+  [selectAllContacts, selectNameFilter],
   (contacts, filter) => {
-    if (contacts.length > 0) return contacts.filter(contact =>
+    return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   }
